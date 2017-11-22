1580e477bd67dee86bcde1bd33371bb690b38dce
Zachary Lee <zach.lee@concur.com>
2017-11-22 12:46:48 -0800

    Create update() function

--- gilded_rose.py
+++ gilded_rose.py
@@ -4,35 +4,11 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            update(item)
 
 
+# DO NOT MODIFY THIS CLASS.
+# I assume that also means you can't extend or inherit from it either
 class Item:
     def __init__(self, name, sell_in, quality):
         self.name = name
@@ -42,3 +18,32 @@ class Item:
     def __repr__(self): # pragma: no cover
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
 
+
+def update(item):
+    if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+        if item.quality > 0:
+            if item.name != "Sulfuras, Hand of Ragnaros":
+                item.quality = item.quality - 1
+    else:
+        if item.quality < 50:
+            item.quality = item.quality + 1
+            if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                if item.sell_in < 11:
+                    if item.quality < 50:
+                        item.quality = item.quality + 1
+                if item.sell_in < 6:
+                    if item.quality < 50:
+                        item.quality = item.quality + 1
+    if item.name != "Sulfuras, Hand of Ragnaros":
+        item.sell_in = item.sell_in - 1
+    if item.sell_in < 0:
+        if item.name != "Aged Brie":
+            if item.name != "Backstage passes to a TAFKAL80ETC concert":
+                if item.quality > 0:
+                    if item.name != "Sulfuras, Hand of Ragnaros":
+                        item.quality = item.quality - 1
+            else:
+                item.quality = item.quality - item.quality
+        else:
+            if item.quality < 50:
+                item.quality = item.quality + 1
