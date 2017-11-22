ed9896074ba70542959dbd05a006e3c320341cc2
Zachary Lee <zach.lee@concur.com>
2017-11-22 13:07:53 -0800

    Refactor first half of update function

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,3 +1,10 @@
+SULFURAS = 'Sulfuras, Hand of Ragnaros'
+BRIE = 'Aged Brie'
+ETC_TICKETS = 'Backstage passes to a TAFKAL80ETC concert'
+
+SPECIAL_ITEMS = [SULFURAS, BRIE, ETC_TICKETS]
+
+
 class GildedRose(object):
     def __init__(self, items):
         self.items = items
@@ -20,30 +27,45 @@ class Item:
 
 
 def update(item):
-    if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-        if item.quality > 0:
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.quality = item.quality - 1
+    if item.name == SULFURAS:
+        update_sulfuras(item)
+        return
+    if item.name not in SPECIAL_ITEMS:
+        update_normal_item(item)
     else:
         if item.quality < 50:
             item.quality = item.quality + 1
-            if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                if item.sell_in < 11:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
-                if item.sell_in < 6:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
-    if item.name != "Sulfuras, Hand of Ragnaros":
-        item.sell_in = item.sell_in - 1
+            if item.name == ETC_TICKETS:
+                update_etc_tickets(item)
+    item.sell_in = item.sell_in - 1
     if item.sell_in < 0:
         if item.name != "Aged Brie":
             if item.name != "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+                    item.quality = item.quality - 1
             else:
                 item.quality = item.quality - item.quality
         else:
             if item.quality < 50:
                 item.quality = item.quality + 1
+
+def update_sulfuras(item):
+    return
+
+def update_etc_tickets(item):
+    if item.sell_in < 11:
+        if item.quality < 50:
+            item.quality = item.quality + 1
+    if item.sell_in < 6:
+        if item.quality < 50:
+            item.quality = item.quality + 1
+
+def update_special_item(item):
+    if item.name == 'Sulfuras, Hand of Ragnaros':
+        update_sulfuras(item)
+
+def update_normal_item(item):
+    if item.quality > 0:
+        item.quality = item.quality - 1
+
+
