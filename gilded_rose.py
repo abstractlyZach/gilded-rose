240b98ad00bf7f69b116b767f0263f4836bef89e
Zachary Lee <zach.lee@concur.com>
2017-11-22 16:04:19 -0800

    Changed sell-in updating to use -=

--- gilded_rose.py
+++ gilded_rose.py
@@ -42,7 +42,7 @@ def update_special_item(item):
         update_aged_brie(item)
     elif item.name == ETC_TICKETS:
         update_etc_tickets(item)
-    item.sell_in = item.sell_in - 1
+    item.sell_in -= 1
 
 def update_sulfuras(item):
     return
@@ -64,11 +64,10 @@ def update_etc_tickets(item):
     if item.sell_in - 1 <= 0:
         item.quality = 0
 
-
 def update_normal_item(item):
     if item.quality > 0:
         item.quality = item.quality - 1
-    item.sell_in = item.sell_in - 1
+    item.sell_in -= 1
     if item.sell_in < 0:
         item.quality = max(item.quality - 1, MIN_QUALITY)
 
