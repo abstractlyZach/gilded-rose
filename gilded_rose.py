d13d8771d1dcbc5bbbf57a1f3ada2b6b15d7eab8
Zachary Lee <zach.lee@concur.com>
2017-11-22 16:29:44 -0800

    Refactor code for better separation of abstraction

--- gilded_rose.py
+++ gilded_rose.py
@@ -47,29 +47,42 @@ def update_sulfuras(item):
 
 def update_aged_brie(item):
     item.sell_in -= 1
+    item.quality = get_new_brie_quality(item)
+
+def get_new_brie_quality(item):
     quality_to_add = 1
     if is_expired(item):
         quality_to_add = 2
-    item.quality = min(item.quality + quality_to_add, 50)
+    return min(item.quality + quality_to_add, MAX_QUALITY)
 
 def update_etc_tickets(item):
+    item.quality = get_new_etc_tickets_quality(item)
+    # updating sell_in happens at a different phase here because of the original logic of
+    # arriving concerts. change that function if you want to move this statement up.
+    item.sell_in -= 1
+    if is_expired(item):
+        item.quality = 0
+
+def get_new_etc_tickets_quality(item):
     quality_to_add = 1
     if item.sell_in < 6:
         quality_to_add = 3
     elif item.sell_in < 11:
         quality_to_add = 2
     new_quality = item.quality + quality_to_add
-    item.quality = min(new_quality, MAX_QUALITY)
-    item.sell_in -= 1
-    if is_expired(item):
-        item.quality = 0
+    return min(new_quality, MAX_QUALITY)
 
 def update_normal_item(item):
-    if item.quality > 0:
-        item.quality = item.quality - 1
     item.sell_in -= 1
+    item.quality = get_new_normal_item_quality(item)
+
+def get_new_normal_item_quality(item):
+    quality_to_lose = 1
     if is_expired(item):
-        item.quality = max(item.quality - 1, MIN_QUALITY)
+        quality_to_lose = 2
+    new_quality = item.quality - quality_to_lose
+    return max(new_quality, MIN_QUALITY)
+
 
 def is_expired(item):
     return item.sell_in < 0
