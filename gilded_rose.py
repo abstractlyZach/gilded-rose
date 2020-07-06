5fa3ca3f52579629b9353dc6025459a607db40d0
abstractlyZach <zach3lee@gmail.com>
2020-07-06 11:18:27 -0700

    Black-ify python style

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,9 +1,9 @@
 MIN_QUALITY = 0
 MAX_QUALITY = 50
-SULFURAS = 'Sulfuras, Hand of Ragnaros'
-BRIE = 'Aged Brie'
-ETC_TICKETS = 'Backstage passes to a TAFKAL80ETC concert'
-CONJURED_PREFIX = 'Conjured'
+SULFURAS = "Sulfuras, Hand of Ragnaros"
+BRIE = "Aged Brie"
+ETC_TICKETS = "Backstage passes to a TAFKAL80ETC concert"
+CONJURED_PREFIX = "Conjured"
 
 SPECIAL_ITEMS = [SULFURAS, BRIE, ETC_TICKETS]
 
@@ -25,7 +25,7 @@ class Item:
         self.sell_in = sell_in
         self.quality = quality
 
-    def __repr__(self): # pragma: no cover
+    def __repr__(self):  # pragma: no cover
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
 
 
@@ -37,6 +37,7 @@ def update(item):
     else:
         update_normal_item(item)
 
+
 def update_special_item(item):
     if item.name == SULFURAS:
         update_sulfuras(item)
@@ -45,12 +46,15 @@ def update_special_item(item):
     elif item.name == ETC_TICKETS:
         update_etc_tickets(item)
 
+
 def update_sulfuras(item):
     return
+
 
 def update_aged_brie(item):
     item.sell_in -= 1
     item.quality = get_new_brie_quality(item)
+
 
 def get_new_brie_quality(item):
     quality_to_add = 1
@@ -58,11 +62,13 @@ def get_new_brie_quality(item):
         quality_to_add = 2
     return min(item.quality + quality_to_add, MAX_QUALITY)
 
+
 def update_etc_tickets(item):
     item.sell_in -= 1
     item.quality = get_new_etc_tickets_quality(item)
     if is_expired(item):
         item.quality = 0
+
 
 def get_new_etc_tickets_quality(item):
     quality_to_add = 1
@@ -73,23 +79,28 @@ def get_new_etc_tickets_quality(item):
     new_quality = item.quality + quality_to_add
     return min(new_quality, MAX_QUALITY)
 
+
 def update_conjured_item(item):
     item.sell_in -= 1
     item.quality = get_new_conjured_item_quality(item)
+
 
 def get_new_conjured_item_quality(item):
     quality_to_lose = 2 * get_normal_item_quality_to_lose(item)
     new_quality = item.quality - quality_to_lose
     return max(new_quality, MIN_QUALITY)
 
+
 def update_normal_item(item):
     item.sell_in -= 1
     item.quality = get_new_normal_item_quality(item)
+
 
 def get_new_normal_item_quality(item):
     quality_to_lose = get_normal_item_quality_to_lose(item)
     new_quality = item.quality - quality_to_lose
     return max(new_quality, MIN_QUALITY)
+
 
 def get_normal_item_quality_to_lose(item):
     if is_expired(item):
@@ -97,6 +108,6 @@ def get_normal_item_quality_to_lose(item):
     else:
         return 1
 
+
 def is_expired(item):
     return item.sell_in < 0
-
--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -4,53 +4,53 @@ import gilded_rose
 
 
 quality_test_cases = (
-    ('expected_quality', 'item', 'comment'),
+    ("expected_quality", "item", "comment"),
     [
-        (0, gilded_rose.Item('foo', 10, 0), "idk what i'm doing"),
-        (49, gilded_rose.Item('foo', 10, 50), ''),
-        (49, gilded_rose.Item('foo', 50, 50), ''),
-        (50, gilded_rose.Item('Aged Brie', 50, 50), ''),
-        (80, gilded_rose.Item('Sulfuras, Hand of Ragnaros', 50, 80), ''),
-        (25, gilded_rose.Item('Sulfuras, Hand of Ragnaros', 50, 25), ''),
-        (26, gilded_rose.Item('Backstage passes to a TAFKAL80ETC concert', 50, 25), ''),
-        (11, gilded_rose.Item('Backstage passes to a TAFKAL80ETC concert', 50, 10), ''),
-        (12, gilded_rose.Item('Backstage passes to a TAFKAL80ETC concert', 10, 10), ''),
-        (41, gilded_rose.Item('Aged Brie', 50, 40), ''),
-        (50, gilded_rose.Item('Backstage passes to a TAFKAL80ETC concert', 10, 49), ''),
-        (50, gilded_rose.Item('Backstage passes to a TAFKAL80ETC concert', 5, 49), ''),
-        (48, gilded_rose.Item('Backstage passes to a TAFKAL80ETC concert', 5, 45), ''),
-        (48, gilded_rose.Item('foo', 0, 50), ''),
-        (42, gilded_rose.Item('Aged Brie', 0, 40), ''),
-        (50, gilded_rose.Item('Aged Brie', 0, 50), ''),
-        (0, gilded_rose.Item('Backstage passes to a TAFKAL80ETC concert', 0, 45), ''),
-        (0, gilded_rose.Item('foo', 0, 0), ''),
-        (80, gilded_rose.Item('Sulfuras, Hand of Ragnaros', -1, 80), ''),
-    ]
+        (0, gilded_rose.Item("foo", 10, 0), "idk what i'm doing"),
+        (49, gilded_rose.Item("foo", 10, 50), ""),
+        (49, gilded_rose.Item("foo", 50, 50), ""),
+        (50, gilded_rose.Item("Aged Brie", 50, 50), ""),
+        (80, gilded_rose.Item("Sulfuras, Hand of Ragnaros", 50, 80), ""),
+        (25, gilded_rose.Item("Sulfuras, Hand of Ragnaros", 50, 25), ""),
+        (26, gilded_rose.Item("Backstage passes to a TAFKAL80ETC concert", 50, 25), ""),
+        (11, gilded_rose.Item("Backstage passes to a TAFKAL80ETC concert", 50, 10), ""),
+        (12, gilded_rose.Item("Backstage passes to a TAFKAL80ETC concert", 10, 10), ""),
+        (41, gilded_rose.Item("Aged Brie", 50, 40), ""),
+        (50, gilded_rose.Item("Backstage passes to a TAFKAL80ETC concert", 10, 49), ""),
+        (50, gilded_rose.Item("Backstage passes to a TAFKAL80ETC concert", 5, 49), ""),
+        (48, gilded_rose.Item("Backstage passes to a TAFKAL80ETC concert", 5, 45), ""),
+        (48, gilded_rose.Item("foo", 0, 50), ""),
+        (42, gilded_rose.Item("Aged Brie", 0, 40), ""),
+        (50, gilded_rose.Item("Aged Brie", 0, 50), ""),
+        (0, gilded_rose.Item("Backstage passes to a TAFKAL80ETC concert", 0, 45), ""),
+        (0, gilded_rose.Item("foo", 0, 0), ""),
+        (80, gilded_rose.Item("Sulfuras, Hand of Ragnaros", -1, 80), ""),
+    ],
 )
 
 sell_in_test_cases = (
-    ('expected_sell_in', 'item', 'comment'),
+    ("expected_sell_in", "item", "comment"),
     [
-        (9, gilded_rose.Item('foo', 10, 0), 'cool item'),
-        (9, gilded_rose.Item('foo', 10, 50), ''),
-        (100, gilded_rose.Item('foo', 101, 50), ''),
-        (49, gilded_rose.Item('Aged Brie', 50, 50), ''),
-        (50, gilded_rose.Item('Sulfuras, Hand of Ragnaros', 50, 50), ''),
-        (50, gilded_rose.Item('Sulfuras, Hand of Ragnaros', 50, 25), ''),
-        (49, gilded_rose.Item('Backstage passes to a TAFKAL80ETC concert', 50, 25), ''),
-        (49, gilded_rose.Item('Backstage passes to a TAFKAL80ETC concert', 50, 10), ''),
-        (9, gilded_rose.Item('Backstage passes to a TAFKAL80ETC concert', 10, 10), ''),
-        (49, gilded_rose.Item('Aged Brie', 50, 40), ''),
-        (9, gilded_rose.Item('Backstage passes to a TAFKAL80ETC concert', 10, 49), ''),
-        (4, gilded_rose.Item('Backstage passes to a TAFKAL80ETC concert', 5, 49), ''),
-        (4, gilded_rose.Item('Backstage passes to a TAFKAL80ETC concert', 5, 45), ''),
-        (-1, gilded_rose.Item('foo', 0, 50), ''),
-        (-1, gilded_rose.Item('Aged Brie', 0, 40), ''),
-        (-1, gilded_rose.Item('Aged Brie', 0, 50), ''),
-        (-1, gilded_rose.Item('Backstage passes to a TAFKAL80ETC concert', 0, 45), ''),
-        (-1, gilded_rose.Item('foo', 0, 0), ''),
-        (-1, gilded_rose.Item('Sulfuras, Hand of Ragnaros', -1, 80), ''),
-    ]
+        (9, gilded_rose.Item("foo", 10, 0), "cool item"),
+        (9, gilded_rose.Item("foo", 10, 50), ""),
+        (100, gilded_rose.Item("foo", 101, 50), ""),
+        (49, gilded_rose.Item("Aged Brie", 50, 50), ""),
+        (50, gilded_rose.Item("Sulfuras, Hand of Ragnaros", 50, 50), ""),
+        (50, gilded_rose.Item("Sulfuras, Hand of Ragnaros", 50, 25), ""),
+        (49, gilded_rose.Item("Backstage passes to a TAFKAL80ETC concert", 50, 25), ""),
+        (49, gilded_rose.Item("Backstage passes to a TAFKAL80ETC concert", 50, 10), ""),
+        (9, gilded_rose.Item("Backstage passes to a TAFKAL80ETC concert", 10, 10), ""),
+        (49, gilded_rose.Item("Aged Brie", 50, 40), ""),
+        (9, gilded_rose.Item("Backstage passes to a TAFKAL80ETC concert", 10, 49), ""),
+        (4, gilded_rose.Item("Backstage passes to a TAFKAL80ETC concert", 5, 49), ""),
+        (4, gilded_rose.Item("Backstage passes to a TAFKAL80ETC concert", 5, 45), ""),
+        (-1, gilded_rose.Item("foo", 0, 50), ""),
+        (-1, gilded_rose.Item("Aged Brie", 0, 40), ""),
+        (-1, gilded_rose.Item("Aged Brie", 0, 50), ""),
+        (-1, gilded_rose.Item("Backstage passes to a TAFKAL80ETC concert", 0, 45), ""),
+        (-1, gilded_rose.Item("foo", 0, 0), ""),
+        (-1, gilded_rose.Item("Sulfuras, Hand of Ragnaros", -1, 80), ""),
+    ],
 )
 
 
@@ -69,22 +69,36 @@ def test_sell_in_after_update_quality(expected_sell_in, item, comment):
     inn.update_quality()
     assert item.sell_in == expected_sell_in
 
+
 conjured_item_test_cases = (
-    ('expected_sell_in', 'expected_quality', 'base_name', 'starting_sell_in',
-     'starting_quality', 'comment'),
+    (
+        "expected_sell_in",
+        "expected_quality",
+        "base_name",
+        "starting_sell_in",
+        "starting_quality",
+        "comment",
+    ),
     [
-        (100, 24, 'potato', 101, 26, ''),
-        (45, 2, 'pizza', 46, 4, ''),
-        (-1, 4, 'pasta', 0, 8, 'expired pasta :('),
-        (50, 0, 'water', 51, 1, 'quality goes below 0'),
-        (-90, 0, 'turkey leg', -89, 3, 'super expired')
-    ]
+        (100, 24, "potato", 101, 26, ""),
+        (45, 2, "pizza", 46, 4, ""),
+        (-1, 4, "pasta", 0, 8, "expired pasta :("),
+        (50, 0, "water", 51, 1, "quality goes below 0"),
+        (-90, 0, "turkey leg", -89, 3, "super expired"),
+    ],
 )
 
+
 @pytest.mark.parametrize(*conjured_item_test_cases)
-def test_conjured_items(expected_sell_in, expected_quality, base_name,
-                        starting_sell_in, starting_quality, comment):
-    item_name = 'Conjured {}'.format(base_name)
+def test_conjured_items(
+    expected_sell_in,
+    expected_quality,
+    base_name,
+    starting_sell_in,
+    starting_quality,
+    comment,
+):
+    item_name = "Conjured {}".format(base_name)
     item = gilded_rose.Item(item_name, starting_sell_in, starting_quality)
     inn = gilded_rose.GildedRose([item])
     inn.update_quality()
